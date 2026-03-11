db1f209240e5ebff229b605c23ae8229b495b51f
kulnor <pascal@codata.org>
2026-03-10 21:47:18 -0600

    feat: add avatar to navigation bar

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     // Pass-through copy for assets
     eleventyConfig.addPassthroughCopy("src/assets/js");
+    eleventyConfig.addPassthroughCopy("src/assets/img");
     eleventyConfig.addPassthroughCopy("src/assets/docs");
     eleventyConfig.addPassthroughCopy("src/assets/css/main.css");
 
